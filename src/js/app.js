995f0bdd25227c3b24affa6b84adb925e8fb56ac
Aixhua <167004871+auix15@users.noreply.github.com>
2024-09-04 18:25:38 +0000

    FirstTry

--- src/js/app.js
+++ src/js/app.js
@@ -24,19 +24,24 @@ import "../style/index.css";
  */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
+  // here we ask the logical questions to make decisions on how to build the hl
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  const name = variables.name == null ? "Nombre" : variables.name;
+  const lastName = variables.lastName == null ? "Apellido" : variables.lastName;
+  const role = variables.role == null ? "Tu rol" : variables.role;
+  const country = variables.country == null ? "País" : variables.country;
+  const city = variables.city == null ? "Ciudad" : variables.city;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
